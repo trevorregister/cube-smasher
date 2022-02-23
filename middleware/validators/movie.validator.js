8eab638bdcf0a2749c6d90e53daa4c268eee2674
trevorregister <trevor.register@pivotinteractives.com>
2022-02-23 10:14:48 -0500

    added movie validator

---
+++ middleware/validators/movie.validator.js
@@ -0,0 +1,34 @@
+const Joi = require('joi')
+const express = require('express')
+const router = express.Router()
+
+
+exports.createMovie = function (req, res, next){ 
+    const JoiSchema = Joi.object({
+    
+        name: Joi.string()
+        .trim()
+        .min(2)
+        .max(30)
+        .required(),
+
+        yearReleased: Joi.number()
+        .greater(1878)
+        .integer()
+        .less(new Date().getFullYear()+1)
+
+    })
+
+    const options = {abortEarly: false}
+
+    const {error, value} = JoiSchema.validate(req.body, options)
+    if (error) return res.status(422).send(`Validation error: ${error.details.map(x => x.message).join(',')}`)
+
+    else req.body = value
+    next()
+
+
+}
+
+
+
--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const moviesController = require('../controllers/moviesController')
+const movieValidator = require('../middleware/validators/movie.validator')
 
 router.get('/', moviesController.getMovies)
 //router.get('/:slug', moviesController.getMovie)
 router.get('/:id', moviesController.getMovieById)
-router.post('/', moviesController.createMovie)
+router.post('/', movieValidator.createMovie, moviesController.createMovie)
 //router.patch('/:name', moviesController.updateMovie)
 //router.delete('/:name', deletemovie)
 
