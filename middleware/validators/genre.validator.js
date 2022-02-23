cbeaf97294f117a5cdd250c9e61f76b95353de75
trevorregister <trevor.register@pivotinteractives.com>
2022-02-23 11:37:59 -0500

    added updateGenre validator

--- middleware/validators/genre.validator.js
+++ middleware/validators/genre.validator.js
@@ -25,6 +25,25 @@ exports.createGenre = function (req, res, next){
 
 }
 
+exports.updateGenre = function (req, res, next){
+    const JoiSchema = Joi.object({
+
+        newName: Joi.string()
+        .trim()
+        .required()
+        .min(2)
+        .max(30)
+    })
+
+    const options = {abortEarly: false}
+    const {error, value} = JoiSchema.validate(req.body, options)
+    if (error) return res.status(422).send(`Validation error: ${error.details.map(x=>x.message).join(',')}`)
+
+    else req.body = value
+    next()
+
+}
+
 //const testGenre = {name: "Act??ion", slug:"ac33&tion", createdAt: Date.now()}
 
 //validateGenre(testGenre)
--- routes/genres.js
+++ routes/genres.js
@@ -7,7 +7,7 @@ router.get('/', genresController.getGenres)
 //router.get('/:slug', genresController.getGenre)
 router.get('/:id', genresController.getGenreById)
 router.post('/', genreValidator.createGenre, genresController.createGenre)
-router.patch('/:name', genresController.updateGenre) //update with slug
+router.patch('/:name', genreValidator.updateGenre, genresController.updateGenre) //update with slug
 //router.delete('/:name', deleteGenre)
 
 module.exports = router
