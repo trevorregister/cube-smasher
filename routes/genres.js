955a1c6e7f5bb95a13b6cc8a0e5c133d4a5c0637
trevorregister <trevor.register@pivotinteractives.com>
2022-02-23 10:00:16 -0500

    updated for validator

--- routes/genres.js
+++ routes/genres.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const genresController = require('../controllers/genresController')
+const genreValidator = require('../middleware/validators/genre.validator')
 
 router.get('/', genresController.getGenres)
 //router.get('/:slug', genresController.getGenre)
 router.get('/:id', genresController.getGenreById)
-router.post('/', genresController.createGenre)
+router.post('/', genreValidator.createGenre, genresController.createGenre)
 router.patch('/:name', genresController.updateGenre) //update with slug
 //router.delete('/:name', deleteGenre)
 
