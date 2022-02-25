7b20c4706c62147ed7294a82fff963ef65ef5746
trevorregister <trevor.register@pivotinteractives.com>
2022-02-25 13:36:33 -0500

    user registration validation

---
+++ middleware/validators/user.validator.js
@@ -0,0 +1,41 @@
+const Joi = require('joi')
+
+exports.registerUser = function (req, res, next){
+
+    const JoiSchema = Joi.object({
+        firstName: Joi.string()
+        .trim()
+        .min(1)
+        .max(30)
+        .required(),
+
+        lastName: Joi.string()
+        .trim()
+        .min(1)
+        .max(30)
+        .required(),
+
+        email: Joi.string()
+        .trim()
+        .min(1)
+        .max(30)
+        .email()
+        .required(),
+
+        password: Joi.string()
+        .trim()
+        .required(),
+
+        role: Joi.string()
+        .trim()
+        .required()
+    })
+
+    const options = {abortEarly: false}
+
+    const {error, value} = JoiSchema.validate(req.body, options)
+    if (error) return res.status(422).send(`Validation error: ${error.details.map(x=>x.message).join(',')}`)
+
+    else req.body = value
+    next()
+}
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
+const userValidator = require('../middleware/validators/user.validator')
 
-router.post('/register', usersController.registerUser)
+router.post('/register', userValidator.registerUser, usersController.registerUser)
 router.post('/login', usersController.login)
 router.post('/rental', usersController.createRental)
 router.patch('/rental/check-in', usersController.checkInRental)
