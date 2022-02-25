50e4453188be6135e9c83b6f6a13df7a02a6c717
trevorregister <trevor.register@pivotinteractives.com>
2022-02-25 09:24:04 -0500

    appropriate status codes

--- controllers/genresController.js
+++ controllers/genresController.js
@@ -17,7 +17,7 @@ exports.getGenres = async function(req, res){
 // exports.getGenre = async function  (req,res){
 //     try {
 //         const genre = await Genre.findOne({"slug":req.params.slug})
-//         if(!genre) return res.status(400).send('Genre not found.')
+//         if(!genre) return res.status(404).send('Genre not found.')
 
 //         return res.status(200).send(genre)
 //     }
@@ -30,7 +30,7 @@ exports.getGenreById = async function (req,res){
 
     try {
         const genre = await Genre.findOne({"_id":req.params.id})
-        if(!genre) return res.status(400).send(`Genre with ${req.params.id} not found`)
+        if(!genre) return res.status(404).send(`Genre with ${req.params.id} not found`)
         return res.status(200).send(genre)
     }
     catch(error){
@@ -43,7 +43,7 @@ exports.createGenre = async function (req,res){
     try {
         var genre = await Genre.findOne({"name":req.body.name})
 
-        if(genre) return res.status(400).send('Genre already exists')
+        if(genre) return res.status(404).send('Genre already exists')
         var genre = new Genre({
             name: req.body.name,
             slug: slugify(req.body.name.toLowerCase()),
@@ -66,7 +66,7 @@ exports.updateGenre = async function (req,res){
 
     try {
         var genre = await Genre.findOne({"name":name})
-        if(!genre) return res.status(400).send(` ${name} does not exist`)
+        if(!genre) return res.status(404).send(` ${name} does not exist`)
 
         var newNameCheck = await Genre.findOne({"name":newName})
         if (newNameCheck) return res.status(403).send(`${newName} already exists`)
